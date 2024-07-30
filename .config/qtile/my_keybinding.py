e710af6446dc23b2278fec15b57e207964daa5ee
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-07-30 14:40:41 +0900

    remap(config/qtile): remap keybinding

--- .config/qtile/my_keybinding.py
+++ .config/qtile/my_keybinding.py
@@ -1,3 +1,4 @@
+import os
 from libqtile.config import Key
 from libqtile.lazy import lazy
 
@@ -7,18 +8,44 @@ def get_my_keybinding(groups):
     alterkey = "mod1"
     terminal = "xfce4-terminal"
 
+    HOME = os.path.expanduser('~')
+     
+
     keys = [
+        Key([], "Print", lazy.spawn("flameshot gui"), desc='Launch screenshot'),
+        Key(["control"], "Escape", lazy.spawn("playerctl play-pause"), desc='Media play/pause'),
+        Key([alterkey], "Return", lazy.spawn(terminal), desc="Launch terminal"),
         Key([alterkey], "delete", lazy.window.kill(), desc="Kill focused window"),
+        Key(
+            [alterkey],
+            "backslash",
+            lazy.spawncmd(),
+            desc="Spawn a command using a prompt widget",
+        ),
+        Key(
+            [alterkey],
+            "f",
+            lazy.window.toggle_fullscreen(),
+            desc="Toggle fullscreen on the focused window",
+        ),
+        Key(
+            [alterkey],
+            "tab",
+            lazy.layout.next(),
+            desc="Move window focus to other window",
+        ),
+        Key([alterkey], "escape", lazy.next_layout(), desc="Toggle between layouts"),
+        # Rofi
+        # Key([mod], "r", lazy.spawn("rofi -show combi"), desc="spawn rofi"),
+        Key([mod], "r", lazy.spawn("rofi -modi drun -show drun"), desc="spawn rofi"),
+        #
+        Key([mod], "equal", lazy.layout.grow(), desc="Grow window"),
+        Key([mod], "minus", lazy.layout.shrink(), desc="Shrink window"),
         # Switch between windows
         Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
         Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
         Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
         Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-        Key(
-            [mod], "space", lazy.layout.next(), desc="Move window focus to other window"
-        ),
-        # Rofi
-        Key([mod], "r", lazy.spawn("rofi -show combi"), desc="spawn rofi"),
         # Move windows between left/right columns or move up/down in current stack.
         # Moving out of range in Columns layout will create new column.
         Key(
@@ -37,8 +64,6 @@ def get_my_keybinding(groups):
         Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
         # Grow windows. If current window is on the edge of screen and direction
         # will be to screen edge - window would shrink.
-        Key([mod, "control"], "h", lazy.layout.grow(), desc="Grow window"),
-        Key([mod, "control"], "l", lazy.layout.shrink(), desc="Shrink window"),
         Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
         # Toggle between split and unsplit sides of stack.
         # Split = all windows displayed
@@ -50,18 +75,21 @@ def get_my_keybinding(groups):
             lazy.layout.toggle_split(),
             desc="Toggle between split and unsplit sides of stack",
         ),
-        Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
         # Toggle between different layouts as defined below
-        Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
         Key([mod, "shift", "control"], "h", lazy.layout.swap_column_left()),
         Key([mod, "shift", "control"], "l", lazy.layout.swap_column_right()),
         Key([mod, "shift"], "space", lazy.layout.flip()),
         # Utility
         Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
         Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-        Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set Master 3%+")),
-        Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set Master 3%-")),
+        # Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set Master 3%+")),
+        Key([], "XF86AudioRaiseVolume", lazy.spawn(f"{HOME}/.config/qtile/scripts/volume.sh volume_up")),
+        # Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set Master 3%-")),
+        Key([], "XF86AudioLowerVolume", lazy.spawn(f"{HOME}/.config/qtile/scripts/volume.sh volume_down")),
         Key([], "XF86AudioMute", lazy.spawn("amixer set Master toggle")),
+
+        Key([], "XF86MonBrightnessUp", lazy.spawn(f"{HOME}/.config/qtile/scripts/brightness.sh brightness_up"), desc='brightness UP'),
+        Key([], "XF86MonBrightnessDown", lazy.spawn(f"{HOME}/.config/qtile/scripts/brightness.sh brightness_down"), desc='brightness Down'),
     ]
 
     for i in groups:
