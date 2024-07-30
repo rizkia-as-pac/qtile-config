4468b3c41c745df21627bba1b06577e5fb0011e4
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-07-30 14:38:18 +0900

    feat(config/qtile): add script for qtile layout and shell script for brightness and volume control in apply.sh

--- apply.sh
+++ apply.sh
@@ -1,7 +1,15 @@
 cp -R .config/nvim ~/.config/
 cp -R .config/qtile/config.py ~/.config/qtile/
+
 cp -R .config/qtile/my_keybinding.py ~/.config/qtile/
 cp -R .config/qtile/my_bar.py ~/.config/qtile/
+cp -R .config/qtile/my_layout.py ~/.config/qtile/
+
 cp -R .config/qtile/autostart.sh ~/.config/qtile/
+
+cp -R .config/qtile/scripts/brightness.sh ~/.config/qtile/scripts/
+cp -R .config/qtile/scripts/volume.sh ~/.config/qtile/scripts/
+
+
 cp -R .bashrc ~
 
