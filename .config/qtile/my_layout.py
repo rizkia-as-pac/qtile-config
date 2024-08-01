63304aac2e3f8b7571e801a9315844e870dd860a
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-08-01 11:41:34 +0900

    feat(config/qtile): add margin and border for qtile max layout

--- .config/qtile/my_layout.py
+++ .config/qtile/my_layout.py
@@ -5,7 +5,11 @@ def get_my_layout():
     layouts = [
         # layout.MonadTall(margin=8, border_focus="#ffffff", border_normal="#2c5380"),
         layout.MonadTall(margin=10, border_focus="#5294E2", border_width=3),
-        layout.Max(),
+        layout.Max(
+            border_normal="#5294E2",
+            margin=10,
+            border_width=3,
+        ),
         # layout.Columns(border_focus_stack='#d75f5f'),
         # Try more layouts by unleashing below layouts.
         # layout.Stack(num_stacks=2),
