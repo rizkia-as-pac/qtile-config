6610b0aba4f692ebf8b2bad8be2b642df31d686d
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-10-28 21:22:50 +0900

    fix(apply.sh)

--- apply.sh
+++ apply.sh
@@ -1,22 +1,22 @@
-cp -R --parents .config/qtile/config.py ~/.config/qtile/
+cp -R .config/qtile/config.py ~/.config/qtile/
 
-cp -R --parents .config/qtile/my_keybinding.py ~/.config/qtile/
-cp -R --parents .config/qtile/my_layout.py ~/.config/qtile/
-cp -R --parents .config/qtile/my_screen.py ~/.config/qtile/
-cp -R --parents .config/qtile/my_group.py ~/.config/qtile/
+cp -R .config/qtile/my_keybinding.py ~/.config/qtile/
+cp -R .config/qtile/my_layout.py ~/.config/qtile/
+cp -R .config/qtile/my_screen.py ~/.config/qtile/
+cp -R .config/qtile/my_group.py ~/.config/qtile/
 
-cp -R --parents .config/qtile/autostart.sh ~/.config/qtile/
-cp -R --parents .config/qtile/autoreload.sh ~/.config/qtile/
+cp -R .config/qtile/autostart.sh ~/.config/qtile/
+cp -R .config/qtile/autoreload.sh ~/.config/qtile/
 
-cp -R --parents .config/qtile/scripts/brightness.sh ~/.config/qtile/scripts/
-cp -R --parents .config/qtile/scripts/volume.sh ~/.config/qtile/scripts/
+cp -R .config/qtile/scripts/brightness.sh ~/.config/qtile/scripts/
+cp -R .config/qtile/scripts/volume.sh ~/.config/qtile/scripts/
 
-cp -R --parents .config/qtile/assets/wallpaper.png ~/.config/qtile/assets/
-cp -R --parents .config/qtile/assets/icon.png ~/.config/qtile/assets/
+cp -R .config/qtile/assets/wallpaper.png ~/.config/qtile/assets/
+cp -R .config/qtile/assets/icon.png ~/.config/qtile/assets/
 
-cp -R --parents .config/rofi ~/.config/
-cp -R --parents .config/mimeapps.list ~/.config/
+cp -R .config/rofi ~/.config/
+cp -R .config/mimeapps.list ~/.config/
 
-cp -R --parents ./shell_config ~/
-cp -R --parents .bashrc ~
+cp -R ./shell_config ~/
+cp -R .bashrc ~
 source ~/.bashrc
