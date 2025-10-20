d98a0d9e8aa9ac905c2497286817b9f12897f3b8
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2025-10-20 09:20:39 +0900

    feat(cleanup): remove obsolete configuration files and scripts
    - Deleted unnecessary configuration files including .Xresources, .gtkrc-2.0, and README.md.
    - Removed scripts related to backup and setup (e.g., backup-i3.sh, setup.sh).
    - Cleared out unused assets (e.g., screenshots, wallpaper, icon).
    - Updated apply.sh to simplify configuration copying and removed redundant script calls.
    
    This cleanup improves repository maintainability and eliminates legacy configurations that are no longer in use. No functionality was affected by this change as these files were not part of the active setup.

--- apply.sh
+++ apply.sh
@@ -1,22 +1,10 @@
-cp -R .config/qtile/config.py ~/.config/qtile/
+# echo -e "\e[33mEnabling Login Manager (SDDM)\e[0m"
+# sudo systemctl enable sddm.service
+# sudo systemctl start sddm.service
+sudo pacman -S --needed --noconfirm - < packages-needed.txt
 
-cp -R .config/qtile/my_keybinding.py ~/.config/qtile/
-cp -R .config/qtile/my_layout.py ~/.config/qtile/
-cp -R .config/qtile/my_screen.py ~/.config/qtile/
-cp -R .config/qtile/my_group.py ~/.config/qtile/
-cp -R .config/qtile/constant.py ~/.config/qtile/
-cp -R .config/qtile/urxvtc.sh ~/.config/qtile/
 
-cp -R .config/qtile/autostart.sh ~/.config/qtile/
-cp -R .config/qtile/autoreload.sh ~/.config/qtile/
 
-cp -R .config/qtile/scripts/brightness.sh ~/.config/qtile/scripts/
-cp -R .config/qtile/scripts/volume.sh ~/.config/qtile/scripts/
-
-cp -R .config/qtile/assets/wallpaper.png ~/.config/qtile/assets/
-cp -R .config/qtile/assets/icon.png ~/.config/qtile/assets/
-
+cp -R .config/qtile ~/.config/
 cp -R .config/rofi ~/.config/
 cp -R .config/mimeapps.list ~/.config/
-
-cp -R ./shell_config ~/
--- backup-i3.sh
+++
@@ -1,6 +0,0 @@
-mv ~/.config/dunst ~/.backup-config/dunst
-mv ~/.config/gtk-3.0 ~/.backup-config/gtk-3.0
-mv ~/.config/rofi ~/.backup-config/rofi
-mv ~/.config/xfce4 ~/.backup-config/xfce4
-mv ~/.Xresources ~/.backup-config/.Xresources
-mv ~/.gtkrc-2.0 ~/.backup-config/.gtkrc-2.0
